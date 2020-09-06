4a5c732964cb94dddfa8b283649d73069a77b6db
bingfengzhanchi <52697799+bingfengzhanchi@users.noreply.github.com>
2020-09-06 13:51:45 +0800

    Update dirtycow.c

--- dirtycow.c
+++ dirtycow.c
@@ -1,33 +1,3 @@
-//
-// This exploit uses the pokemon exploit of the dirtycow vulnerability
-// as a base and automatically generates a new passwd line.
-// The user will be prompted for the new password when the binary is run.
-// The original /etc/passwd file is then backed up to /tmp/passwd.bak
-// and overwrites the root account with the generated line.
-// After running the exploit you should be able to login with the newly
-// created user.
-//
-// To use this exploit modify the user values according to your needs.
-//   The default is "DarkHQ".
-//
-// Original exploit (dirtycow's ptrace_pokedata "pokemon" method):
-//   https://github.com/dirtycow/dirtycow.github.io/blob/master/pokemon.c
-//
-// Compile with:
-//   gcc -pthread dirty.c -o dirty -lcrypt
-//
-// Then run the newly create binary by either doing:
-//   "./dirty" or "./dirty my-new-password"
-//
-// Afterwards, you can either "su DarkHQ" or "ssh DarkHQ@..."
-//
-// DON'T FORGET TO RESTORE YOUR /etc/passwd AFTER RUNNING THE EXPLOIT!
-//   mv /tmp/passwd.bak /etc/passwd
-//
-// Exploit adopted by Christian "DarkHQ" Mehlmauer
-// https://DarkHQ.at
-//
-
 #include <fcntl.h>
 #include <pthread.h>
 #include <string.h>
